0cea55e74ea5a8e4d9fc70ac1cf7683541de4f3d
Marina Ustinovich <marishka0509@mail.ru>
2023-03-30 09:33:15 +0300

    fixed tests

--- src/js/__test__/getDateNow.test.js
+++ src/js/__test__/getDateNow.test.js
@@ -1,21 +1,29 @@
 import getDateNow from '../getDateNow';
+import localeRu from 'dayjs/locale/ru';
+
+const dayjs = require('dayjs'); // import dayjs from 'dayjs'
 
 test(('should be 2023-03-29 for now'), () => {
-  expect(getDateNow().dateNow).toBe('2023-03-29');
+  const expected = dayjs().locale(localeRu).format('YYYY-MM-DD');
+  expect(getDateNow().dateNow).toBe(expected);
 });
 
 test(('should be 29.03 for now'), () => {
-  expect(getDateNow().dateForSpanDay).toBe('29.03');
+  const expected = dayjs().locale(localeRu).format('DD.MM');
+  expect(getDateNow().dateForSpanDay).toBe(expected);
 });
 
 test(('should be 31.03 for now'), () => {
-  expect(getDateNow().dateForSpanAddTwoDay).toBe('31.03');
+  const expected = dayjs().add(2, 'day').format('DD.MM');
+  expect(getDateNow().dateForSpanAddTwoDay).toBe(expected);
 });
 
 test(('should be 30.03 for now'), () => {
-  expect(getDateNow().dateForSpanAddDay).toBe('30.03');
+  const expected = dayjs().add(1, 'day').format('DD.MM');
+  expect(getDateNow().dateForSpanAddDay).toBe(expected);
 });
 
 test(('should be 2023-07-29 for now'), () => {
-  expect(getDateNow().max).toBe('2023-07-29');
+  const expected = dayjs().add(4, 'month').format('YYYY-MM-DD');
+  expect(getDateNow().max).toBe(expected);
 });
